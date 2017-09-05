4ed543120cd0521858602d34e865e0e884f09885
jiang-home <0532yangya@163.com>
2017-09-05 22:51:35 +0800

    1

---
+++ JianzhiOffer/sqrt.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+
+# given a number, get its sqrt.
+
+def get_sqrt(a, prec):
+    low_val = 0
+    high_val = a
+    mid_val = low_val+(high_val-low_val)/2
+    while abs(mid_val-a)>prec:    # 如果不符合， 一直找
+        if
+
+
+    return mid_val
+
