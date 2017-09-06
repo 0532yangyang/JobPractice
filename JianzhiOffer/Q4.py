3bebc1f1d56b7815c0bcb359fb04af330f8446dd
jiang-home <0532yangya@163.com>
2017-09-06 23:09:15 +0800

    Q4

--- JianzhiOffer/Q4.py
+++ JianzhiOffer/Q4.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
 
-def replace_space(raw_str, new):
+def replace_space(raw_str, replace):
     # count query
     tar = ' '
     cnt = raw_str.count(tar)
-    diff = len(new) - len(tar)
+    diff = len(replace) - len(tar)
     postfix = ['0']*cnt*diff
     new_str = raw_str + postfix
     print new_str
     j = len(new_str) - 1
     i = len(raw_str) - 1
     while j>=0:
-        if new_str[j] == tar:
-            for k in range(len(new))[::-1]:
-                new_str[j] =  new_str[k]
+        if raw_str[i] == tar:
+            for k in range(len(replace))[::-1]:
+                new_str[j] = replace[k]
                 j -= 1
+            i -= 1
         else:
             new_str[j] = new_str[i]
             i -= 1
